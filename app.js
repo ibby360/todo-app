522fafbfae9d0a5bb9ca6e2135389900ba7068a4
Ibby360 <ibrahimramadhan360@outlook.com>
2021-03-26 20:13:26 +0300

    Add getTodo function

--- app.js
+++ app.js
@@ -108,3 +108,15 @@ function removeLocalTodos(todo) {
     todos.splice(todos.indexOf(todoIndex), 1);
     localStorage.setItem("todos", JSON.stringify(todos));
 }
+
+function removeLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
