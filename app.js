cd72204179c46b85888a03ebf708ab9596fe5689
Ibby360 <ibrahimramadhan360@outlook.com>
2021-03-26 20:12:58 +0300

    Add removeLocalTodo function

--- app.js
+++ app.js
@@ -96,3 +96,15 @@ function saveLocalTodos(todo) {
     todos.push(todo);
     localStorage.setItem("todos", JSON.stringify(todos));
 }
+
+function removeLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
