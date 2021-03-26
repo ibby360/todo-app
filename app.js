5eeb4f3c7858fd9d23757f34c6e3b44daab8469e
Ibby360 <ibrahimramadhan360@outlook.com>
2021-03-26 20:11:29 +0300

    Add filter function

--- app.js
+++ app.js
@@ -61,3 +61,27 @@ function deleteTodo(e) {
         console.log(todo);
     }
 }
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+        }
+    });
+}
